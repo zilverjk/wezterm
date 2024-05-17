be27c69c6e1cd7fb4291649d2710700f5c354fc0
André Cruz <acruztalavera@gmail.com>
2024-05-17 17:40:24 -0500

    Se seteo no mostrar warnings de glyphs para fonts

--- wezterm.lua
+++ wezterm.lua
@@ -5,13 +5,13 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- Launch WezTerm as fullscreen window
 wezterm.on("gui-startup", function(cmd)
-	local _, _, window = mux.spawn_window(cmd or {})
-	window:gui_window():toggle_fullscreen()
+  local _, _, window = mux.spawn_window(cmd or {})
+  window:gui_window():toggle_fullscreen()
 end)
 
 config.native_macos_fullscreen_mode = true
@@ -22,26 +22,27 @@ config.window_close_confirmation = "NeverPrompt"
 config.scrollback_lines = 3000
 config.enable_wayland = false
 config.check_for_updates = false
+config.warn_about_missing_glyphs = false
 
 -- Font
 config.font = wezterm.font({ family = "MonoLisaNerdFontCompleteMono Nerd Font" }, { weight = "Bold" })
 config.font_size = 12.0
 config.keys = {
-	{
-		key = "-",
-		mods = "CTRL",
-		action = wezterm.action.DisableDefaultAssignment,
-	},
-	{
-		key = "=",
-		mods = "CTRL",
-		action = wezterm.action.DisableDefaultAssignment,
-	},
-	{
-		key = "|",
-		mods = "CTRL",
-		action = wezterm.action.DisableDefaultAssignment,
-	},
+  {
+    key = "-",
+    mods = "CTRL",
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = "=",
+    mods = "CTRL",
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = "|",
+    mods = "CTRL",
+    action = wezterm.action.DisableDefaultAssignment,
+  },
 }
 
 return config
