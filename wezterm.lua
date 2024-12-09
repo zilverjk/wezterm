09b83a9fd49364170081b4ea421361a53983fa71
André Cruz <acruztalavera@gmail.com>
2024-12-08 22:08:37 -0500

    Corrección de fuente monolisa con ligaduras en family

--- wezterm.lua
+++ wezterm.lua
@@ -15,21 +15,21 @@ config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_rate = 0
 config.max_fps = 120
 config.window_padding = {
-  left = 2,
-  right = 2,
-  top = 15,
-  bottom = 0,
+	left = 2,
+	right = 2,
+	top = 15,
+	bottom = 0,
 }
 -- config.scrollback_lines = 3000
 config.check_for_updates = false
-config.font = wezterm.font({ family = "MonoLisaNerdFontCompleteMonoNerdFont Nerd Font" })
+config.font = wezterm.font({ family = "Liga MonoLisaNerdFontCompleteMonoNer" })
 config.font_size = 13.0
 config.keys = {
-  {
-    key = "n",
-    mods = "SHIFT|CTRL",
-    action = wezterm.action.ToggleFullScreen,
-  },
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
 }
 
 return config
